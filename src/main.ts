a91908118d626113ad2b93618b1cd1783cb8bcb3
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2024-06-07 21:14:25 -0300

    feat: add Swagger (OpenAPI)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as compression from 'compression';
 import helmet from 'helmet';
 
@@ -8,6 +9,14 @@ import { AppPipeTransform } from './app.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('QuikDev Documentation')
+    .setVersion('1.0.0')
+    .setDescription('This is an API QuikDev')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new AppPipeTransform());
   app.useGlobalPipes(new ValidationPipe());
